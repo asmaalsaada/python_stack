efa814215be85706bbc8c9301784bc16c426ebfb
asmaalasaada <asma.alsaada96@gmail.com>
2021-05-07 13:31:57 +0300

    try

---
+++ _python/python_fundamentals/Functions Intermediate 1.py
@@ -0,0 +1,27 @@
+import random 
+def randInt(min='', max=100):
+    num = random.random() * max
+    print (num) # for test
+    return round(num) 
+print(randInt()) 
+
+
+def randInt(min='', max=100):
+    num = random.random() * max
+    print (num) # for test
+    return round(num) 
+print (randInt(max=50)) 
+
+def randInt(min='', max=100):
+    num = random.random() * max
+    print (num) # for test
+    return round(num) 
+print(randInt(min=50)) 
+
+def randInt(min='', max=100):
+    num = random.random() * max
+    print (num) # for test
+    return round(num) 
+print(randInt(min=50, max=500))  
+
+
