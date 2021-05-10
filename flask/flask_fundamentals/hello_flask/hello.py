509c9ddfdfd75c902fd3ff7ca3a84057e8871e78
asmaalasaada <asma.alsaada96@gmail.com>
2021-05-10 14:54:24 +0300

    flask day 1

--- flask/flask_fundamentals/hello_flask/hello.py
+++ flask/flask_fundamentals/hello_flask/hello.py
@@ -1,31 +1,53 @@
-from flask import Flask, render_template    # Import Flask to allow us to create our app
-app = Flask(__name__)    # Create a new instance of the Flask class called "app"
-@app.route("/")          # The "@" decorator associates this route with the function immediately following
+# from flask import Flask
+# app = Flask(__name__)
+# @app.route('/')
 # def hello_world():
-# def index():
-#     return render_template("index.html", phrase="hello" , times=5)
-    # return render_template("index.html") 
-    # return 'Hello World!'  # Return the string 'Hello World!' as a response
-# if __name__=="__main__":   # Ensure this file is being run directly and not from a different module    
-#     app.run(debug=True)    # Run the app in debug mode.
+#     return 'Hello World!'
+# if __name__=="__main__":
+#     app.run(debug=True)
 
-# import statements, maybe some other routes
-    
-# Import Flask to allow us to create our app
-# app = Flask(__name__)     
-@app.route("/") 
-def hello(): 
-    print("in the hello function")
-    return render_template("name.html")
-    # return "Hello!"
-@app.route("/<name>, <times>") 
-def hello_person(name,times): 
-    print ("*" * 80)
-    print(name)
-    print("in the hello_person function")
-    return render_template("name.html", some_name=name, num_times=int(times))
-    # return f"Hello ,{name}!"
-if __name__=="__main__":  
-    app.run(debug=True) 
-# app.run(debug=True) should be the very last statement! 
+# from flask import Flask
+# app = Flask(__name__)
+# @app.route('/dojo')
+# def hello_world():
+#     return 'Dojo!'
+# if __name__=="__main__":
+#     app.run(debug=True)
+
+# from flask import Flask
+# app = Flask(__name__)
+# @app.route('/say/flask')
+# def hello_flask():
+#     return 'Hi Flask!'
+
+# @app.route('/say/micheal')
+# def hello_micheal():
+#     return 'Hi Micheal!'
+
+# @app.route('/say/john')
+# def hello_john():
+#     return 'Hi John!'
+
+# if __name__=="__main__":
+#     app.run(debug=True)
+
+
+# from flask import Flask
+# app = Flask(__name__)
+# @app.route('/repeat/35/hello')
+# def repeat_hello():
+#     return int("35")* "hello \n"
+
+# @app.route('/repeat/80/bye')
+# def repeat_bye():
+#     return int("80")* "bye \n"
+
+# @app.route('/repeat/99/dogs')
+# def repeat_dogs():
+#     return int("99")* "dog \n"
+# if __name__=="__main__":
+#     app.run(debug=True)
+
+
+
 
