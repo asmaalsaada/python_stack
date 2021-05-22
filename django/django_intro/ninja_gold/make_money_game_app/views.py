30e8c7121bfbb4b2639f7f6bf9a63220bfe1dd25
asmaalasaada <asma.alsaada96@gmail.com>
2021-05-22 15:49:58 +0300

    Ninja Gold

--- django/django_intro/ninja_gold/make_money_game_app/views.py
+++ django/django_intro/ninja_gold/make_money_game_app/views.py
@@ -19,25 +19,25 @@ def process(request):
     if request.POST['inp'] == 'farm':
         x=random.randint(10,20)
         request.session['farm'] = x # generate a random number
-        farm_log="you earned "+str(x)+" gold from farm " + request.session['time']
+        farm_log="You earned "+str(x)+" gold from farm " + " ( " + request.session['time'] + " ) "
         request.session['farm_log'].append(farm_log)
         request.session['gold'] += request.session['farm']
     if request.POST['inp'] == 'cave':
         j=random.randint(5,10)
         request.session['cave'] = j # generate a random number
-        cave_log="you earned "+str(j)+" gold from cave " + request.session['time']
+        cave_log="You earned "+str(j)+" gold from cave "+ " ( " + request.session['time'] + " ) "
         request.session['cave_log'].append(cave_log)
         request.session['gold'] += request.session['cave']
     if request.POST['inp'] == 'house':
         f=random.randint(2,5)
         request.session['house'] = f # generate a random number
-        house_log="you earned "+str(f)+" gold from house " + request.session['time']
+        house_log="You earned "+str(f)+" gold from house " + " ( " + request.session['time'] + " ) "
         request.session['house_log'].append(house_log)
         request.session['gold'] += request.session['house']
     if request.POST['inp'] == 'casino':
         d=random.randint(-50,50)
         request.session['casino'] = d # generate a random number
-        casino_log="you got "+str(d)+" gold from casino " + request.session['time']
+        casino_log="You got "+str(d)+" gold from casino " + " ( " + request.session['time'] + " ) "
         request.session['casino_log'].append(casino_log)
         request.session['gold'] += request.session['casino']
     return redirect('/')
